81cd52b45b4c60c33cc84f453b56fd031fda08c5
GrandChris <christiansteinbrecher@gmx.at>
2020-08-17 16:20:36 +0200

    test 5

---
+++ .github/workflows/build.ps1
@@ -0,0 +1,3 @@
+&"C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\MSBuild\Current\Bin\MSBuild.exe" VulkanErruption\VulkanErruption.sln -target:VulkanErruption
+# &"C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\MSBuild\Current\Bin\MSBuild.exe" VulkanErruption\VulkanErruption.sln -target:VulkanErruption
+
